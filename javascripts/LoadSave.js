6241366d6ba4f776fe549fc862f813d02604c6f4
The-MathsMaster <50160441+The-MathsMaster@users.noreply.github.com>
2020-03-18 16:22:51 +0800

    Fixed saving (hopefully)

--- javascripts/LoadSave.js
+++ javascripts/LoadSave.js
@@ -3,22 +3,18 @@ var savePlayer = JSON.parse(sP);
 if (localStorage.getItem("player") !== null) {
 	Object.keys(initPlayer).forEach(function(key) {
 		if (savePlayer[key] != undefined) {
-			if ((savePlayer[key].m != undefined && savePlayer[key].e != undefined) || key == "timePlayed" || key == "notation" || key == "theme") {
-				if (initPlayer[key] == undefined) {
-					delete savePlayer.key;
-					console.log("Deleted excess " + key);
-					alert("Deleted excess value " + key)
+			if (initPlayer[key] == undefined) {
+				if (typeof(savePlayer[key]) != typeof(initPlayer[key])) {
+					console.log(key + "invalid");
+					savePlayer[key] = initPlayer[key];
+					console.log("Value " + key + " fixed");
 				} else {
 					console.log(key + "found");
 				}
 			} else {
-				console.log(key + "invalid");
-				if (key == "money") {
-					savePlayer.money = new Decimal(1);
-				} else {
-					savePlayer[key] = new Decimal(0);
-				}
-				console.log("Value " + key + " fixed");
+				delete savePlayer.key;
+				console.log("Deleted excess " + key);
+				alert("Deleted excess value " + key);
 			}
 		} else {
 			savePlayer[key] = initPlayer[key];
@@ -38,12 +34,7 @@ if (localStorage.getItem("player") !== null) {
 				}
 			}
 		});
-		update();
-		for (var i = 0; i < 1000; i++) {
-			player.money = player.money.add((player.hacks[0].div(1000000).mul(multi.hacks)).mul(((new Date()).getTime) - savePlayer.timePlayed));
-			player.hacks[0] = player.hacks[0].add(((player.hackers[0].div(2000000).mul(multi.hackers).add(player.trainers[0].div(400000).mul(multi.trainers).add(player.masters[0].div(80000).mul(multi.masters)))).mul(((new Date()).getTime) - savePlayer.timePlayed)));			player.hackers[0] = player.hackers[0].add((player.trainers[0].div(10000000).mul(multi.trainers).add(player.masters[0].div(2000000).mul(multi.masters))).mul(((new Date()).getTime) - savePlayer.timePlayed));
-			player.trainers[0] = player.trainers[0].add((player.masters[0].div(25000000).mul(multi.masters)).mul(((new Date()).getTime) - savePlayer.timePlayed));
-		}
+		simulateTime((new Date).getTime() - savePlayer.timePlayed);
 		document.getElementById("away").style.display = "initial";
 		document.getElementById("awayMessage").innerHTML = "While you were away, your money gained " + formatNotation(new Decimal(savePlayer.money.log10() - player.money.log10())) + " Orders of Magnitude.";
 	} else {
