00fbb5a25cc0288f63ccffc99b9f1d845041b227
The-MathsMaster <chemistryfrancium@gmail.com>
2020-06-22 19:00:50 +0800

    nerfed cultivator

--- javascripts/cultivator.js
+++ javascripts/cultivator.js
@@ -1,4 +1,4 @@
 function grow() {
 	player.plantPicked = player.plantPicked.add(player.plantPicked.min(player.container).pow(player.containerLevel/5+1));
 }
-setInterval(grow, 500);
+setInterval(grow, 1000);
