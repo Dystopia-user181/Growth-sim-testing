e2897e11e40138993f96b333178db4302f682695
The-MathsMaster <chemistryfrancium@gmail.com>
2020-06-22 16:48:08 +0800

    Fixed more bug

--- javascripts/ui.js
+++ javascripts/ui.js
@@ -5,7 +5,7 @@ function updateUI() {
 	$("price").innerText = toSci(plantSell/10, 2);
 	(player.tutorial.unlockedSell) ? $("sellplant").style.visibility = "visible" : $("sellplant").style.visibility = "hidden";
 	(player.tutorial.unlockedSell) ? $("moneys").style.visibility = "visible" : $("moneys").style.visibility = "hidden";
-	(player.tutorial.unlockedSell) ? $("priceSpan").style.visibility = "visible" : $("price").style.visibility = "hidden";
+	(player.tutorial.unlockedSell) ? $("priceSpan").style.visibility = "visible" : $("priceSpan").style.visibility = "hidden";
 	player.version = "0.0.0.2";
 }
 setInterval(updateUI, 50);
