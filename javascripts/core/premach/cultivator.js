0b7a6d81e07ac5d794258ecd661ea605948d8f56
dystopias-user <50160441+Dystopia-user181@users.noreply.github.com>
2020-09-02 22:19:34 +0800

    fixed conflicts

--- javascripts/core/premach/cultivator.js
+++ javascripts/core/premach/cultivator.js
@@ -1,31 +1,31 @@
 function upCvt() {
 	if (player.money.lt(Decimal.pow(2e3, player.containerLevel.add(1)))) return;
-	player.containerLevel = player.containerLevel.add(1);
+	player.cvt.level = player.cvt.level.add(1);
 	player.money = player.money.sub(Decimal.pow(2e3, player.containerLevel));
 }
 function buyCvt() {
 	if (player.container.bought.lte(200)) {
 		if (player.money.lt(player.container.bought.mul(5).add(20))) return;
 		player.money = player.money.sub(player.container.bought.mul(5).add(20));
-		player.container.total = player.container.total.add(1);
-		player.container.bought = player.container.bought.add(1);
+		player.cvt.total = player.cvt.total.add(1);
+		player.cvt.bought = player.cvt.bought.add(1);
 	} else {
 		if (player.money.lt(Decimal.pow(1.01, player.container.bought.sub(200)).mul(1000))) return;
 		player.money = player.money.sub(Decimal.pow(1.01, player.container.bought.sub(200)).mul(1000));
-		player.container.total = player.container.total.add(1);
-		player.container.bought = player.container.bought.add(1);
+		player.cvt.total = player.ctv.total.add(1);
+		player.cvt.bought = player.cvt.bought.add(1);
 	}
 }
 function buyMaxCvt(money) {
-	if (player.money.lt(Decimal.pow(1.01, player.container.bought.sub(200)).mul(1000))) return;
-	var cvtamt = new Decimal(money.add(Decimal.pow(1.01, player.container.bought.sub(200)).mul(101000)).div(101000).log(1.01)).sub(player.container.bought.sub(200)).floor().max(0);
-	player.money = player.money.sub(Decimal.pow(1.01, player.container.bought.add(cvtamt).sub(200)).mul(101000).sub(Decimal.pow(1.01, player.container.bought.sub(200)).mul(101000))).max(0);
-	player.container.total = player.container.total.add(cvtamt);
-	player.container.bought = player.container.bought.add(cvtamt);
+	if (player.money.lt(Decimal.pow(1.01, player.cvt.bought.sub(200)).mul(1000))) return;
+	var cvtamt = new Decimal(money.add(Decimal.pow(1.01, player.cvt.bought.sub(200)).mul(101000)).div(101000).log(1.01)).sub(player.cvt.bought.sub(200)).floor().max(0);
+	player.money = player.money.sub(Decimal.pow(1.01, player.cvt.bought.add(cvtamt).sub(200)).mul(101000).sub(Decimal.pow(1.01, player.cvt.bought.sub(200)).mul(101000))).max(0);
+	player.cvt.total = player.cvt.total.add(cvtamt);
+	player.cvt.bought = player.cvt.bought.add(cvtamt);
 }
 function buyMaxCvtUp(money) {
-	if (player.money.lt(Decimal.pow(2e3, player.containerLevel).mul(2e3))) return;
-	var cvtupamt = new Decimal(money.log(2e3)).floor().sub(player.containerLevel).max(0);
-	player.money = player.money.sub(Decimal.pow(2e3, player.containerLevel.add(cvtupamt)));
-	player.containerLevel = player.containerLevel.add(cvtupamt);
+	if (player.money.lt(Decimal.pow(2e3, player.cvt.level).mul(2e3))) return;
+	var cvtupamt = new Decimal(money.log(2e3)).floor().sub(player.cvt.level).max(0);
+	player.money = player.money.sub(Decimal.pow(2e3, player.cvt.level.add(cvtupamt)));
+	player.cvt.level = player.cvt.level.add(cvtupamt);
 }
