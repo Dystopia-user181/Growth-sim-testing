cb8a59942e58013ed308fb1e0dc7f8acab8a2ba6
dystopias-user <50160441+Dystopia-user181@users.noreply.github.com>
2020-10-04 22:58:17 +0800

    Fixed bug with not being able to buy cvt, hives, and queens due to miscalculation of v-bind:disabled

--- javascripts/core/premach/bee.js
+++ javascripts/core/premach/bee.js
@@ -2,7 +2,7 @@ function sellHoney(amt=200) {
 	amt = new Decimal(amt);
 	if (player.honey.lt(amt)) return;
 	player.honey = player.honey.sub(amt);
-	player.money = player.money.add(Decimal.pow(1.6, player.marketing).mul(0.05).mul(amt));
+	player.money = player.money.add(Decimal.pow(1.6, player.marketing).mul(0.1).mul(amt));
 	if (player.money.gte(40)) player.tutorial.unlockedPot = true;
 	if (player.money.gte(500)) player.tutorial.unlockedMarketing = true;
 	if (player.money.gte(1e14) && !player.tutorial.unlockedMachine) player.navigation.tab = "Machine";
