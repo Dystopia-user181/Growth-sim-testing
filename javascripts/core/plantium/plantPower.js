f286b2ffb10fc157da34d1ac43f496383dc6f66d
dystopias-user <50160441+Dystopia-user181@users.noreply.github.com>
2020-10-06 20:14:04 +0800

    Redid generators

--- javascripts/core/plantium/plantPower.js
+++ javascripts/core/plantium/plantPower.js
@@ -1,6 +1,11 @@
-function assign(amt=1) {
-	amt = new Decimal(amt);
-	if (player.plantium.lt(amt)) return;
-	player.plantium = player.plantium.sub(amt.floor());
-	player.generators = player.generators.add(amt.floor());
+function assign() {
+	if (player.plantium.lt(Decimal.pow(2, player.generators))) return;
+	player.plantium = player.plantium.sub(Decimal.pow(2, player.generators));
+	player.generators = player.generators.add(1);
+}
+function assignMax(money) {
+	if (money.lt(Decimal.pow(2, player.generators))) return;
+	var genamt = new Decimal(money.add(Decimal.pow(2, player.generators).mul(2)).div(2).log(2)).sub(player.generators).floor().max(0);
+	player.plantium = player.plantium.sub(Decimal.pow(2, player.generators.add(genamt)).mul(2).sub(Decimal.pow(2, player.generators).mul(2))).max(0);
+	player.generators = player.generators.add(genamt);
 }
