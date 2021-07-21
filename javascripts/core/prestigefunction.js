9492996a16a11d2eaeaa513499051a5d7db95a40
dystopias-user <50160441+Dystopia-user181@users.noreply.github.com>
2021-07-21 10:18:30 +0800

    removed console debugging for a non-broken func

--- javascripts/core/prestigefunction.js
+++ javascripts/core/prestigefunction.js
@@ -1,13 +1,9 @@
 function prestige(keystokeep) {
 	keystokeep.push("navigation", "version", "tutorial", "option", "display");
 	var keys = {};
-	console.log(keystokeep);
 	keystokeep.forEach(function (item) {
-		console.log(keys[item]);
 		keys[item] = runParse(JSON.parse(JSON.stringify(player[item])), initPlayer[item]);
 	});
-	console.log(keystokeep);
-	console.log(keys);
 	player = reseter(player, initPlayer);
 	keystokeep.forEach(function (key) {
 		player[key] = keys[key];
